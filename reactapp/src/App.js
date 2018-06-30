fe7a24b74bc13cf4ae20899e14e4f5269b0a6393
Thon Ly <thonly@heartbank.ceo>
2018-06-30 14:27:13 -0700

    Add form to transfer tokens

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -6,10 +6,7 @@ import web3 from "./web3";
 import token from "./token";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { balance: "" };
-  }
+  state = { balance: "", address: "", amount: "", status: "" };
 
   async componentDidMount() {
     const accounts = await web3.eth.getAccounts();
@@ -18,6 +15,22 @@ class App extends Component {
     this.setState({ balance });
   }
 
+  transfer = async event => {
+    event.preventDefault();
+    this.setState({ status: "Transfer in progress..." });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await token.methods
+        .transfer(this.state.address, this.state.amount)
+        .send({ from: accounts[0] });
+      const balance = await token.methods.balanceOf(accounts[0]).call();
+      this.setState({ status: "Transfer success!", balance });
+    } catch (error) {
+      this.setState({ status: error.message });
+    }
+  };
+
   render() {
     return (
       <div className="App">
@@ -25,7 +38,25 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Token Contract</h1>
         </header>
-        <p className="App-intro">Your balance is {this.state.balance}</p>
+        <h3 className="App-intro">Your balance is {this.state.balance}</h3>
+        <form onSubmit={this.transfer}>
+          <div>
+            <label>Address:</label>
+            <input
+              value={this.state.address}
+              onChange={event => this.setState({ address: event.target.value })}
+            />
+          </div>
+          <div>
+            <label>Amount:</label>
+            <input
+              value={this.state.amount}
+              onChange={event => this.setState({ amount: event.target.value })}
+            />
+          </div>
+          <button>Transfer</button>
+        </form>
+        <h5>{this.state.status}</h5>
       </div>
     );
   }
